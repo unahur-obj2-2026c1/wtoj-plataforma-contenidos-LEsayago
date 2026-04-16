8c4d10469559bf53b65039a68405addfb08e4c0c
LEsayago <lucas.e.sayago@gmail.com>
2026-04-16 20:14:52 -0300

    Que tema los Test 2

--- src/test/java/ar/edu/unahur/obj2/w2j/usuarios/UsuarioTest.java
+++ src/test/java/ar/edu/unahur/obj2/w2j/usuarios/UsuarioTest.java
@@ -4,11 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import ar.edu.unahur.obj2.w2j.contenidos.Pelicula;
+
 public class UsuarioTest {
     @Test
     void dadoUnUsuarioConElPlanBasico_SECalculaCorrectamenteSuCostoMensual() {
         Usuario usuario = new Usuario();
          assertEquals(5.0, usuario.costoMensual());
+    }
+
+    @Test
+    void dadoUnUsuarioConElPlanBasicoYExcedentes_SECalculaCorrectamenteSuCostoMensual() {
+        Usuario usuarioVicioso = new Usuario();
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+        usuarioVicioso.verConteniido(new Pelicula("lo que el viento", 10.0));
+         assertEquals(15.0, usuarioVicioso.costoMensual());
     }
 
     @Test
