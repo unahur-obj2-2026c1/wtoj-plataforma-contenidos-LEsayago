c0f624ebecb34e5e3b66e42b8ee6e57748c03242
LEsayago <lucas.e.sayago@gmail.com>
2026-04-16 19:36:05 -0300

    Vemos

--- src/main/java/ar/edu/unahur/obj2/w2j/usuarios/Usuario.java
+++ src/main/java/ar/edu/unahur/obj2/w2j/usuarios/Usuario.java
@@ -1,5 +1,50 @@
 package ar.edu.unahur.obj2.w2j.usuarios;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unahur.obj2.w2j.contenidos.Contenido;
+import ar.edu.unahur.obj2.w2j.planes.IPlan;
+import ar.edu.unahur.obj2.w2j.planes.PlanBasico;
+
 public class Usuario {
+
+    private IPlan plan = new PlanBasico(10);
+    private List<Contenido> contenidos = new ArrayList<>();
+    
+    public Usuario() {
+    }
+
+    public Usuario(IPlan plan) {
+        this.plan = plan;
+    }
+
+
+    public Usuario(List<Contenido> contenidos) {
+        this.contenidos = contenidos;
+    }
+
+
+    public Usuario(IPlan plan, List<Contenido> contenidos) {
+        this.plan = plan;
+        this.contenidos = contenidos;
+    }
+
+
+    public List<Contenido> getContenidos() {
+        return contenidos;
+    }
+
+    public void verConteniido(Contenido contenido) {
+        contenidos.add(contenido);
+    }
+
+    public void setPlan(IPlan plan) {
+        this.plan = plan;
+    }
+
+    public IPlan getPlan() {
+        return plan;
+    }
 
 }
