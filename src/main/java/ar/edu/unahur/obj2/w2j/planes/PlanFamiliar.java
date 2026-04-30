880779395d62a30f1badf30d17db055f0f6e8cce
LEsayago <lucas.e.sayago@gmail.com>
2026-04-30 20:50:26 -0300

    Strategy terminado, no hereda planpremium

--- src/main/java/ar/edu/unahur/obj2/w2j/planes/PlanFamiliar.java
+++ src/main/java/ar/edu/unahur/obj2/w2j/planes/PlanFamiliar.java
@@ -1,6 +1,7 @@
 package ar.edu.unahur.obj2.w2j.planes;
 
 public class PlanFamiliar extends PlanConLimite {
+    private static final Double descuento = 0.15;
 
    public PlanFamiliar (Integer limite){
     super(limite);
@@ -8,8 +9,8 @@ public class PlanFamiliar extends PlanConLimite {
    
    @Override
    protected Double aplicarAjuste(Double total) {
-       return total * 0.85;
+       return total * (1 - descuento);
    }
-    
+
 
 }
