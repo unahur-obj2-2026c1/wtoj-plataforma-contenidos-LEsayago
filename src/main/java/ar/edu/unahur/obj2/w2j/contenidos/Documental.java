00e46cf488b3bc75e0e45440a2d90d6f8104ea26
LEsayago <lucas.e.sayago@gmail.com>
2026-04-30 19:01:14 -0300

    probando

--- src/main/java/ar/edu/unahur/obj2/w2j/contenidos/Documental.java
+++ src/main/java/ar/edu/unahur/obj2/w2j/contenidos/Documental.java
@@ -7,7 +7,7 @@ public class Documental extends Contenido {
     public Documental(String titulo, Double costoBase) {
         super(titulo, costoBase);
     }
-
+// podriamos definir directamente una variable de clase
     @Override
     public Double doCosto() {
         return IDRA.getInstance().getValor();
